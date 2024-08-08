2502e254a2e14dc82189b30aaae8e4ba5f372f1d
changehee <oche.edu@gmail.com>
2024-08-08 13:32:09 +0900

    Accounts Signup Get요청 기능

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = CustomUserCreationForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'signup.html', context)
