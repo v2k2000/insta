e91ccafc973f69370e42b7ea13d3e985e2606a2b
changehee <oche.edu@gmail.com>
2024-08-12 15:09:39 +0900

    accounts profile 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from .models import User
+
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
@@ -42,3 +44,14 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('posts:index')
+
+
+def profile(request, username):
+    user_info = User.objects.get(username=username)
+
+    context = {
+        'user_info': user_info,
+        # 'user': 로그인한유저
+    }
+
+    return render(request, 'profile.html', context)
